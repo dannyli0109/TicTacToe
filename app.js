8cf1ca0298b57632e59ba0f27a432797532bec9b
Danny Li <dannyli0109@gmail.com>
2017-06-13 17:23:12 +1000

    win combinations

--- app.js
+++ app.js
@@ -4,7 +4,55 @@ var board
 
 var game = {
   board:[],
-  players:[]
+  players:[],
+  winPatterns: [],
+  checkGameOver: function() {
+    for (var i = 0; i <  game.board.length; i++) {
+      for (var j = 0; j < game.board[0].length; j++) {
+        
+      }
+    }
+  },
+  determineWinPattern: function() {
+    var returnPattern = [];
+    var winPattern = [];
+    // row wins
+    for (var i = 0; i <  game.board.length; i++) {
+      winPattern = [];
+      for (var j = 0; j < game.board[0].length; j++) {
+        winPattern.push(game.board[i][j])
+      }
+      returnPattern.push(winPattern)
+    }
+
+    // col wins
+    for (var j = 0; j < game.board[0].length; j++) {
+      winPattern = []
+      for (var i = 0; i <  game.board.length; i++) {
+        winPattern.push(game.board[i][j])
+      }
+      returnPattern.push(winPattern)
+    }
+
+    // diag wins (no metter how big the board is, only two possibilities)
+    winPattern = [];
+    for (var i = 0; i <  game.board.length; i++) {
+      for (var j = 0; j < game.board[0].length; j++) {
+        if (i == j) {
+          winPattern.push(game.board[i][j])
+        }
+      }
+    }
+    returnPattern.push(winPattern)
+
+    winPattern = [];
+    var lastIndex = game.board[0].length - 1
+    for (var i = 0; i <  game.board.length; i++) {
+      winPattern.push(game.board[i][lastIndex - i])
+    }
+    returnPattern.push(winPattern)
+    return returnPattern
+  }
 }
 
 var initBoard = function(rows, cols) {
@@ -24,6 +72,7 @@ game.players.push(new Player("player1", 0))
 game.players.push(new Player("player2", 1))
 game.board = initBoard(3,3)
 game.turn = 0
+game.winPatterns = game.determineWinPattern()
 
 function setup() {
   createCanvas(301, 301)
@@ -43,9 +92,10 @@ function mousePressed() {
   for (var i = 0; i <  game.board.length; i++) {
     for (var j = 0; j < game.board[0].length; j++) {
       if (game.board[i][j].contains(mouseX, mouseY)) {
-        game.board[i][j].occupiedCellPlayer = game.players[game.turn % game.players.length]
-        game.turn ++
-
+        if (!game.board[i][j].occupiedCellPlayer) {
+          game.board[i][j].occupiedCellPlayer = game.players[game.turn % game.players.length]
+          game.turn ++
+        }
       }
     }
   }
